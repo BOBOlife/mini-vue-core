87375b14bbc6a68a9860799678a030cc88ab8fa0
BoWang <bbw1996@qq.com>
2022-03-28 23:17:01 +0800

    update:  reactive & effect

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,58 @@
+class ReactiveEffect {
+  private _fn: any;
+
+
+  constructor(fn, public scheduler?) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+}
+
+
+const targetMap = new Map();
+
+export function track(target, key) {
+  // target ->  key ->  dep
+  let depsMap = targetMap.get(target);
+
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+let activeEffect;
+export function effect(fn, options: any = {}) {
+  // fn
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+
+  _effect.run();
+
+  const runner = _effect.run.bind(_effect);
+  return runner;
+}
