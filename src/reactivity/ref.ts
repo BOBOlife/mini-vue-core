952b1e9ee71a8fdeaccc5d5efb74f02be2a558f5
BoWang <bbw1996@qq.com>
2022-04-16 18:27:08 +0800

    update: 实现 isRef & unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   public dep: any;
   private _rawValue: any;
+  public __v_isRef: boolean = true;
   constructor(value) {
     this._rawValue = value;
     this._value = convert(value);
@@ -42,4 +43,12 @@ export function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
 }
