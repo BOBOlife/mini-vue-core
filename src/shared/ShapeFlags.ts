686fadf23ac755225f125c81c5112c5ba3b6fdff
BoWang <bbw1996@qq.com>
2022-07-10 17:40:00 +0800

    fix: unit test

--- src/shared/ShapeFlags.ts
+++ src/shared/ShapeFlags.ts
@@ -4,3 +4,21 @@ export const enum ShapeFlags {
   TEXT_CHILDREN = 1 << 2, // 0100
   ARRAY_CHILDREN = 1 << 3 // 1000 
 }
+// 之前这种方式
+// ShapeFlags.stateful_component = 1
+
+// 使用位运算
+//  ｜ 两位为0，才为0
+//  &  两位为1 ，才为1
+
+// 修改  ｜
+// 0000
+// 0001
+//——————
+// 0001
+
+// 查找 &
+// 0001
+// 0001
+// ——————
+// 0001
