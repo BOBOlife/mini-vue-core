146710aba2cc537160e1293deeb893e0ccbc1566
BoWang <bobo@BO.local>
2023-11-21 15:11:41 +0800

    feat:实现解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -9,13 +14,42 @@ function parseChildren(context) {
   const nodes: any = [];
 
   let node;
-  if (context.source.startsWith("{{")) {
+  const s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
 
   return nodes;
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  // <div></div>
+  // <div></div>
+  // 1.解析div
+  // 2.删除处理完成的代码
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
@@ -31,10 +65,10 @@ function parseInterpolation(context) {
   const rawContentLength = closeIndex - openDelimiter.length;
 
   const rawContent = context.source.slice(0, rawContentLength);
-  const content = rawContent.trim();
+  const content = rawContent.trim(); // {{}} 里面的内容去空格
 
   advanceBy(context, rawContentLength + closeDelimiter.length);
-
+  // 抽象语法树
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -1,5 +1,6 @@
 import { NodeTypes } from "../src/ast";
 import { baseParse } from "../src/parse";
+
 describe("Parse", () => {
   describe("interpolation", () => {
     test("simple interpolation", () => {
@@ -11,6 +12,17 @@ describe("Parse", () => {
           type: NodeTypes.SIMPLE_EXPRESSION,
           content: "message",
         },
+      });
+    });
+  });
+
+  describe("element", () => {
+    it("simple element div", () => {
+      const ast = baseParse("<div></div>");
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: "div",
       });
     });
   });
